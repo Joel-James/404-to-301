4e20d57627fd8b6b5c507485faf4b0653adc7fcf
Joel James <me@joelsays.com>
2018-06-28 23:27:29 +0530

    Do not translate html

--- includes/public/class-jj4t3-404-email.php
+++ includes/public/class-jj4t3-404-email.php
@@ -210,7 +210,7 @@ class JJ4T3_404_Email {
 		$message .= '</tr>';
 		$message .= '</table>';
 		// Who sent me this alert?
-		$message .= '<p>' . __( 'Alert sent by the <strong>404 to 301</strong> plugin for WordPress.', JJ4T3_DOMAIN ) . '</p>';
+		$message .= '<p>' . sprintf( __( 'Alert sent by the %s404 to 301%s plugin for WordPress.', JJ4T3_DOMAIN ), '<strong>', '</strong>' ) . '</p>';
 
 		/**
 		 * Filter to alter email content.
