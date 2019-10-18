97c08e764cad76b5c77b434a094a3af5cf54c06d
Joel James <me@joelsays.com>
2019-10-18 13:22:11 +0530

    👌 Addded single log item class

--- core/models/class-log-item.php
+++ core/models/class-log-item.php
@@ -6,13 +6,10 @@ namespace DuckDev\WP404\Models;
 defined( 'WPINC' ) || die;
 
 use WP_Error;
-use WP_REST_Server;
-use WP_REST_Request;
-use WP_REST_Response;
-use DuckDev\WP404\Utils\Abstracts\Endpoint;
+use DuckDev\WP404\Utils\Abstracts\Base;
 
 /**
- * Logs functionality REST endpoint.
+ * Single log item model class.
  *
  * @link       https://duckdev.com
  * @since      4.0.0
@@ -21,7 +18,7 @@ use DuckDev\WP404\Utils\Abstracts\Endpoint;
  *
  * @author     Joel James <me@joelsays.com>
  */
-class Logs extends Endpoint {
+class Log_Item extends Base {
 
 	/**
 	 * API endpoint for the current endpoint.
@@ -30,228 +27,77 @@ class Logs extends Endpoint {
 	 *
 	 * @since 4.0.0
 	 */
-	private $endpoint = '/logs/';
+	protected $id;
 
 	/**
-	 * Get current API endpoint url.
+	 * Get the redirect type of current error.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @return string
+	 * @return int
 	 */
-	public function get_url() {
-		return rest_url( $this->get_namespace() . $this->endpoint );
-	}
-
-	/**
-	 * Register the routes for handling settings functionality.
-	 *
-	 * Register routes:
-	 * - v1/settings - GET to get settings.
-	 * - v1/settings - POST, PUT, PATCH to update settings.
-	 *
-	 * @since 4.0.0
-	 */
-	public function register_routes() {
-		// Route to get the logs list.
-		register_rest_route(
-			$this->get_namespace(),
-			$this->endpoint, [
-				[
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => [ $this, 'get_logs' ],
-					'permission_callback' => [ $this, 'permissions_check' ],
-					'args'                => [],
-				],
-			]
-		);
-
-		// Route to get a single log.
-		register_rest_route(
-			$this->get_namespace(),
-			$this->endpoint . '(?P<id>\d+)', [
-				[
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => [ $this, 'get_log' ],
-					'permission_callback' => [ $this, 'permissions_check' ],
-					'args'                => [
-						'id' => [
-							'validate_callback' => function ( $param ) {
-								return is_numeric( $param );
-							},
-						],
-					],
-				],
-			]
-		);
-
-		// Route to update a log.
-		register_rest_route(
-			$this->get_namespace(),
-			$this->endpoint . '(?P<id>\d+)', [
-				[
-					'methods'             => WP_REST_Server::EDITABLE,
-					'callback'            => [ $this, 'update_log' ],
-					'permission_callback' => [ $this, 'permissions_check' ],
-					'args'                => [
-						'id' => [
-							'validate_callback' => function ( $param ) {
-								return is_numeric( $param );
-							},
-						],
-					],
-				],
-			]
-		);
-
-		// Route to delete a log.
-		register_rest_route(
-			$this->get_namespace(),
-			$this->endpoint . '(?P<id>\d+)', [
-				[
-					'methods'             => WP_REST_Server::DELETABLE,
-					'callback'            => [ $this, 'delete_log' ],
-					'permission_callback' => [ $this, 'permissions_check' ],
-					'args'                => [
-						'id' => [
-							'validate_callback' => function ( $param ) {
-								return is_numeric( $param );
-							},
-						],
-					],
-				],
-			]
-		);
-	}
-
-	/**
-	 * Get the logs list from db.
-	 *
-	 * @param WP_REST_Request $request Request object.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return WP_Error|WP_REST_Response
-	 */
-	public function get_logs( $request ) {
-		// Get the optional params.
-		$page  = $request->get_param( 'page' );
-		$size  = $request->get_param( 'per_page' );
-		$order = $request->get_param( 'order_by' );
-
+	public function get_redirect_type() {
 		// Send response.
-		return $this->get_response( [
-			[
-				'id'       => 1,
-				'path'     => 'test',
-				'date'     => '25-12-2019',
-				'referral' => 'none',
-				'ip'       => '127.0.0.1',
-				'ua'       => 'none',
-			],
-			[
-				'id'       => 2,
-				'path'     => 'test/test',
-				'date'     => '26-15-2019',
-				'referral' => 'none',
-				'ip'       => '127.0.0.1',
-				'ua'       => 'none',
-			],
-			[
-				'id'       => 3,
-				'path'     => 'test/testdg',
-				'date'     => '26-15-2019',
-				'referral' => 'none',
-				'ip'       => '127.0.0.1',
-				'ua'       => 'none',
-			],
-			[
-				'id'       => 4,
-				'path'     => 'testada',
-				'date'     => '26-15-2019',
-				'referral' => 'none',
-				'ip'       => '127.0.0.1',
-				'ua'       => 'none',
-			],
-			[
-				'id'       => 5,
-				'path'     => 'test/teste',
-				'date'     => '26-15-2019',
-				'referral' => 'none',
-				'ip'       => '127.0.0.1',
-				'ua'       => 'none',
-			],
-		] );
+		return 301;
 	}
 
 	/**
-	 * Get a single log item data.
-	 *
-	 * @param WP_REST_Request $request Request object.
+	 * Get the redirect url of current error.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @return WP_Error|WP_REST_Response
+	 * @return int
 	 */
-	public function get_log( $request ) {
-		// Get the log ID.
-		$id = $request->get_param( 'id' );
-
+	public function get_redirect_url() {
 		// Send response.
-		return $this->get_response( [] );
+		return 301;
 	}
 
 	/**
-	 * Update a single log item data.
-	 *
-	 * @param WP_REST_Request $request Request object.
+	 * Get the redirect type of current error.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @return WP_Error|WP_REST_Response
+	 * @return int
 	 */
-	public function update_log( $request ) {
-		// Get the log ID.
-		$id = $request->get_param( 'id' );
-
+	public function get_log_status() {
 		// Send response.
-		return $this->get_response( [] );
+		return true;
 	}
 
 	/**
-	 * Delete a single log item.
-	 *
-	 * @param WP_REST_Request $request Request object.
+	 * Get the redirect type of current error.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @return WP_Error|WP_REST_Response
+	 * @return int
 	 */
-	public function delete_log( $request ) {
-		// Get the log ID.
-		$id = $request->get_param( 'id' );
-
+	public function get_email_status() {
 		// Send response.
-		return $this->get_response( [] );
+		return true;
 	}
 
 	/**
-	 * Check if a given request has access to update a setting.
+	 * Save the current log item to db.
 	 *
-	 * @param WP_REST_Request $request Request object.
+	 * @since 4.0.0
 	 *
 	 * @return bool
 	 */
-	public function permissions_check( $request ) {
-		/**
-		 * Filter to modify settings capability.
-		 *
-		 * @paran string $cap Capability name.
-		 *
-		 * @since 4.0.0
-		 */
-		$settings_cap = apply_filters( '404_to_301_settings_capability', 'manage_options' );
+	public function save() {
+		// Send response.
+		return true;
+	}
 
-		return current_user_can( $settings_cap );
+	/**
+	 * Delete the current log item.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return bool
+	 */
+	public function delete() {
+		// Send response.
+		return true;
 	}
 }
