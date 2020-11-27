a30946a71c0f7d5cf1cbcfdba0d183f0020b8c65
Joel James <me@joelsays.com>
2020-11-27 10:36:17 +0530

    👌 Added new class

--- core/models/class-logs.php
+++ core/models/class-logs.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * The tables model class.
+ * The error logs model class.
  *
- * This class handles the database queries for creating
- * and updating tables.
+ * This class handles the database queries for error logs
+ * management.
  *
  * @author     Joel James <me@joelsays.com>
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @copyright  Copyright (c) 2020, Joel James
  * @link       https://duckdev.com/products/404-to-301/
- * @package    Model
+ * @package    Endpoint
  * @since      4.0.0
- * @subpackage Tables
+ * @subpackage Settings
  */
 
 namespace DuckDev\Redirect\Models;
@@ -26,21 +26,7 @@ use DuckDev\Redirect\Utils\Abstracts\Model;
  *
  * @package DuckDev\DD4T3\Models
  */
-class Tables extends Model {
-
-	/**
-	 * Main log table name.
-	 *
-	 * @since 4.0.0
-	 */
-	const LOG_TABLE = '404_to_301';
-
-	/**
-	 * Custom options table for the logs.
-	 *
-	 * @since 4.0.0
-	 */
-	const OPTIONS_TABLE = '404_to_301_options';
+class Logs extends Model {
 
 	/**
 	 * Setup the plugin and register all hooks.
@@ -51,9 +37,11 @@ class Tables extends Model {
 	 *
 	 * @since 1.8.0
 	 *
-	 * @return void
+	 * @return array
 	 */
-	public function create() {
+	public function get_list() {
+		$list = array();
+
 		/**
 		 * Action hook to trigger after initializing all core actions.
 		 *
@@ -61,7 +49,7 @@ class Tables extends Model {
 		 *
 		 * @since 1.8.0
 		 */
-		do_action( 'wpmudev_vids_after_core_init' );
+		return apply_filters( '404_to_301_model_logs_get_list', $list );
 	}
 
 	/**
@@ -100,43 +88,5 @@ class Tables extends Model {
 		 * @since 1.8.0
 		 */
 		return apply_filters( '404_to_301_log_table_schema', $query );
-	}
-
-	/**
-	 * Get the table creation query for the log options table.
-	 *
-	 * This query is going to be used with dbDelta function
-	 * to upgrade the table structure or create new table.
-	 *
-	 * @see   https://codex.wordpress.org/Creating_Tables_with_Plugins
-	 * @since 4.0.0
-	 *
-	 * @return string
-	 */
-	public function options_table_schema() {
-		// Get table name.
-		$table = $this->get_table_name( self::OPTIONS_TABLE );
-
-		$query = "CREATE TABLE $table (
-            id BIGINT NOT NULL AUTO_INCREMENT,
-            date DATETIME NOT NULL,
-            url VARCHAR(512) NOT NULL,
-            ref VARCHAR(512) NOT NULL default '',
-            ip VARCHAR(40) NOT NULL default '',
-            ua VARCHAR(512) NOT NULL default '',
-            redirect VARCHAR(512) NULL default '',
-			options LONGTEXT,
-			status BIGINT NOT NULL default 1,
-            PRIMARY KEY  (id)
-        );";
-
-		/**
-		 * Filter hook to modify the logs options table schema.
-		 *
-		 * @param string $query Query string.
-		 *
-		 * @since 1.8.0
-		 */
-		return apply_filters( '404_to_301_log_options_table_schema', $query );
 	}
 }
