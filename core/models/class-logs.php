090e194548325ca711d72c7b61f81a664bd04e98
Joel James <me@joelsays.com>
2021-07-01 11:52:28 +0530

    📦 Added helper methods for insert and update operations

--- core/models/class-logs.php
+++ core/models/class-logs.php
@@ -29,6 +29,33 @@ use DuckDev\Redirect\Utils\Abstracts\Model;
 class Logs extends Model {
 
 	/**
+	 * Current model table name.
+	 *
+	 * @var string $table Table name.
+	 *
+	 * @since 4.0
+	 */
+	protected $table = '404-to-301';
+
+	/**
+	 * Field names of the table.
+	 *
+	 * @var string[] $fields
+	 *
+	 * @since 4.0.0
+	 */
+	protected $fields = array(
+		'id'       => '%d',
+		'date'     => '%s',
+		'url'      => '%s',
+		'ref'      => '%s',
+		'ip'       => '%s',
+		'ua'       => '%s',
+		'redirect' => '%s', // @deprecated.
+		'status'   => '%s',
+	);
+
+	/**
 	 * Setup the plugin and register all hooks.
 	 *
 	 * Pro version features and not initialized yet, so do not
@@ -59,58 +86,18 @@ class Logs extends Model {
 	 * execute something on this hooks if you are checking for
 	 * Pro version.
 	 *
+	 * @param array $data Data.
+	 *
 	 * @since 1.8.0
 	 *
-	 * @return array
+	 * @return bool
 	 */
 	public function create( $data = array() ) {
-		$list = array();
+		// Can not continue if url is empty.
+		if ( empty( $data['url'] ) ) {
+			return false;
+		}
 
-		/**
-		 * Action hook to trigger after initializing all core actions.
-		 *
-		 * You still need to check if it Pro version or Free.
-		 *
-		 * @since 1.8.0
-		 */
-		return apply_filters( '404_to_301_model_logs_get_list', $list );
-	}
-
-	/**
-	 * Get the table creation query for the main log table.
-	 *
-	 * This query is going to be used with dbDelta function
-	 * to upgrade the table structure or create new table.
-	 *
-	 * @see   https://codex.wordpress.org/Creating_Tables_with_Plugins
-	 * @since 4.0.0
-	 *
-	 * @return string
-	 */
-	public function logs_table_schema() {
-		// Get table name.
-		$table = $this->get_table_name( self::LOG_TABLE );
-
-		$query = "CREATE TABLE $table (
-            id BIGINT NOT NULL AUTO_INCREMENT,
-            date DATETIME NOT NULL,
-            url VARCHAR(512) NOT NULL,
-            ref VARCHAR(512) NOT NULL default '',
-            ip VARCHAR(40) NOT NULL default '',
-            ua VARCHAR(512) NOT NULL default '',
-            redirect VARCHAR(512) NULL default '',
-			options LONGTEXT,
-			status BIGINT NOT NULL default 1,
-            PRIMARY KEY  (id)
-        );";
-
-		/**
-		 * Filter hook to modify the logs table schema.
-		 *
-		 * @param string $query Query string.
-		 *
-		 * @since 1.8.0
-		 */
-		return apply_filters( '404_to_301_log_table_schema', $query );
+		return $this->insert( $data );
 	}
 }
--- core/utils/abstracts/class-model.php
+++ core/utils/abstracts/class-model.php
@@ -27,19 +27,150 @@ defined( 'WPINC' ) || die;
 abstract class Model extends Base {
 
 	/**
+	 * Current model table name.
+	 *
+	 * @var string $table Table name.
+	 *
+	 * @since 4.0
+	 */
+	protected $table;
+
+	/**
+	 * Field names of the table.
+	 *
+	 * @var string[] $fields
+	 *
+	 * @since 4.0.0
+	 */
+	protected $fields = array();
+
+	/**
 	 * Get the table name appending prefix.
 	 *
 	 * Classes can override this by extending it.
 	 *
-	 * @param string $name Table name.
-	 *
-	 * @since 4.0.0
+	 * @since 4.0
 	 *
 	 * @return string
 	 */
-	protected function get_table_name( $name ) {
+	protected function table_name() {
 		global $wpdb;
 
-		return $wpdb->prefix . $name;
+		return $wpdb->prefix . $this->table;
+	}
+
+	/**
+	 * Get the field names of the table.
+	 *
+	 * @since 4.0
+	 *
+	 * @return string[]
+	 */
+	protected function field_names() {
+		return array_keys( $this->fields );
+	}
+
+	/**
+	 * Get the field format string.
+	 *
+	 * @param string $name Field name.
+	 *
+	 * @since 4.0
+	 *
+	 * @return string
+	 */
+	protected function field_format( $name ) {
+		return isset( $this->fields[ $name ] ) ? $this->fields[ $name ] : '%s';
+	}
+
+	/**
+	 * Prepare data to process it with DB.
+	 *
+	 * Allow only the fields defined in the table.
+	 * Get formatting strings for each fields.
+	 *
+	 * @param array $data Data.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return array
+	 */
+	protected function prepare_data( $data ) {
+		$final = array(
+			'values' => array(),
+			'format' => array(),
+		);
+
+		// Data validation.
+		foreach ( $data as $field => $value ) {
+			// Only if allowed.
+			if ( in_array( $field, $this->field_names(), true ) ) {
+				// Get format string.
+				$final['format'][] = $this->field_format( $field );
+				// Get value.
+				$final['values'][ $field ] = $value;
+			}
+		}
+
+		return $final;
+	}
+
+	/**
+	 * Perform data insert to current table.
+	 *
+	 * We will allow only fields added in $fields var.
+	 *
+	 * @param array $data Data.
+	 *
+	 * @since 4.0
+	 *
+	 * @return bool
+	 */
+	protected function insert( $data ) {
+		global $wpdb;
+
+		// Data validation.
+		list( $data, $format ) = $this->prepare_data( $data );
+
+		// Insert data to table.
+		if ( ! empty( $values ) ) {
+			// phpcs:ignore
+			$result = $wpdb->insert( $this->table_name(), $data, $format );
+
+			return ! empty( $result );
+		}
+
+		return false;
+	}
+
+	/**
+	 * Perform data update in current table.
+	 *
+	 * We will allow only fields added in $fields var.
+	 *
+	 * @param array $data  Data.
+	 * @param array $where Where fields.
+	 *
+	 * @since 4.0
+	 *
+	 * @return bool
+	 */
+	protected function update( $data, $where ) {
+		global $wpdb;
+
+		// Get data and formats.
+		list( $data, $format ) = $this->prepare_data( $data );
+		// Get where formats.
+		list( $where, $where_format ) = $this->prepare_data( $where );
+
+		// Update data in table.
+		if ( ! empty( $values ) && ! empty( $where ) ) {
+			// phpcs:ignore
+			$wpdb->update( $this->table_name(), $data, $where, $format, $where_format );
+
+			return ! empty( $result );
+		}
+
+		return false;
 	}
 }
