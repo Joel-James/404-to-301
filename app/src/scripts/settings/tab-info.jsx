17805fcf96eeef844eceb53c594c32af86025309
Joel James <me@joelsays.com>
2022-02-04 12:36:35 +0530

    👌 Updated info page

--- app/src/scripts/settings/tab-info.jsx
+++ app/src/scripts/settings/tab-info.jsx
@@ -13,31 +13,57 @@ export default class TabInfo extends Component {
 
 	render() {
 		return (
-			<PanelBody title={__('Help & Support', '404-to-301')}>
-				<div className="dd4t3-info">
-					<p>{__('Our extensive documentation contains references and guides for most situations you may encounter. We have an active and friendly community on our wp.org forum who may be able to help you figure out the ‘how-tos’ of the 404 to 301 world.', '404-to-301')}</p>
-					<p>{__('If the above options did not help you can reach out to me directly through my contact form. Please note, it may take a few days for me to reply as I am the only developer working on new features, bug fixes, improvements, documentation, customer support etc. after my full time job.', '404-to-301')}</p>
-					<div className="dd4t3-info-button-group">
-						<Button
-							variant="secondary"
-							target="_blank"
-							icon="external"
-							href="https://duckdev.com/docs/404-to-301/"
-						>
-							{__('Documentation', '404-to-301')}
-						</Button>
+			<>
+				<PanelBody title={__('Help & Support', '404-to-301')}>
+					<div className="dd4t3-info">
+						<p>{__('Our extensive documentation contains references and guides for most situations you may encounter. We have an active and friendly community on our wp.org forum who may be able to help you figure out the ‘how-tos’ of the 404 to 301 world.', '404-to-301')}</p>
+						<div className="dd4t3-info-button-group">
+							<Button
+								variant="secondary"
+								target="_blank"
+								icon="admin-page"
+								href="https://duckdev.com/docs/404-to-301/"
+							>
+								{__('Documentation', '404-to-301')}
+							</Button>
 
-						<Button
-							variant="secondary"
-							target="_blank"
-							icon="external"
-							href="https://wordpress.org/support/plugin/404-to-301/"
-						>
-							{__('Support Forums', '404-to-301')}
-						</Button>
+							<Button
+								variant="secondary"
+								target="_blank"
+								icon="groups"
+								href="https://wordpress.org/support/plugin/404-to-301/"
+							>
+								{__('Support Forums', '404-to-301')}
+							</Button>
+						</div>
 					</div>
-				</div>
-			</PanelBody>
+				</PanelBody>
+
+				<PanelBody title={__('About the Author', '404-to-301')}>
+					<div className="dd4t3-info">
+						<p>{__('Hey, I am Joel James, a WordPress developer from Kerala, India. I spend a few hours every week to contribute to open source. I have a few other WordPress plugin which I would recommend you to try out.', '404-to-301')}</p>
+						<div className="dd4t3-info-button-group">
+							<Button
+								variant="secondary"
+								target="_blank"
+								icon="admin-site"
+								href="https://duckdev.com/about/"
+							>
+								{__('About Me', '404-to-301')}
+							</Button>
+
+							<Button
+								variant="secondary"
+								target="_blank"
+								icon="wordpress"
+								href="https://profiles.wordpress.org/joelcj91/"
+							>
+								{__('WP.org Profile', '404-to-301')}
+							</Button>
+						</div>
+					</div>
+				</PanelBody>
+			</>
 		);
 	}
 }
